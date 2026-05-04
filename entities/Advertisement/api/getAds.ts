34c023df34811ed2341b2fe2d12a5fb4247f68c4
pavelbekarev <bekarev.ps@gmail.com>
2026-05-04 17:53:23 +0500

    fix

--- entities/Advertisement/api/getAds.ts
+++ entities/Advertisement/api/getAds.ts
@@ -5,14 +5,15 @@ export async function getAds() {
   // async code to server
   // await new Promise((resolve) => setTimeout(resolve, 500));
 
+  console.debug("!!!");
   try {
-    // const response = await api.get("wp-json/wp/v2/announcements");
     const response = await fetch(
       `${process.env.WP_API}/wp-json/wp/v2/announcements`,
       {
         next: { tags: ["ads"] },
       },
     );
+    console.debug(response);
 
     return await response.json();
   } catch (e) {
