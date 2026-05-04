d0be35c18618328a4f710200f7b0539985b760e5
pavelbekarev <bekarev.ps@gmail.com>
2026-05-04 13:22:24 +0500

    fix fetch data

--- shared/api/axiosInstance.ts
+++ shared/api/axiosInstance.ts
@@ -4,7 +4,7 @@ export const defaultEndpoint = "http://localhost:3000/api";
 export const defaultImageEndpoint = "http://localhost:3000";
 
 export const api = axios.create({
-  baseURL: "https://veravla-edu.ru",
+  baseURL: "http://veravla-edu.ru/",
   headers: {
     "Content-Type": "application/json",
   },
